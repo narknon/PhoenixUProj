7971c526d7ffc0a70fb872e8090407dfaf6127b8
Narknon <narknon0@gmail.com>
2023-02-07 14:41:48 -0500

    edits to project files

---
+++ Source/phoenix.Target.cs
@@ -0,0 +1,22 @@
+﻿using UnrealBuildTool;
+
+public class phoenixTarget : TargetRules {
+	public phoenixTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange(new string[] {
+			"AIPathScheduler",
+			"AvaAnimationShim",
+			"CreatureBehavior",
+			"DbAssetMapper",
+			"DbGateway",
+			"GameScheduler",
+			"OnlineStore",
+			"PersistentData",
+			"Phoenix",
+			"PhoenixSignificanceManager",
+			"SceneRigShim",
+			"SocialDisplay",
+		});
+	}
+}
